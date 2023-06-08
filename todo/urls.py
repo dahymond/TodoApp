e2c9104abc365e3f61402b977d5314b171291dd5
dahymond <ssdahymond@gmail.com>
2023-06-07 23:19:16 -0500

    completed edit task, delete task features

--- todo/urls.py
+++ todo/urls.py
@@ -4,5 +4,12 @@ from . import views
 urlpatterns = [
     path('addTask/', views.addTask, name='addTask'),
     path('mark_as_done/<int:pk>/', views.mark_as_done, name='mark_as_done'),
+
+    #mark as undone
     path('mark_as_undone/<int:pk>/', views.mark_as_undone, name='mark_as_undone'),
+    #edit features
+    path('edit_task/<int:pk>/', views.edit_task, name='edit_task'),
+
+    #Delete Tasks
+    path('delete_task/<int:pk>/', views.delete_task, name='delete_task'),
 ]
